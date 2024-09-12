51a514b3c45b21416835ad241f9c32dad870fb98
FrancoCastillo99 <Francocast_99@hotmail.com>
2024-09-12 20:58:20 -0300

    Login responsive

--- front-end-ecommerce/src/Login.jsx
+++ front-end-ecommerce/src/Login.jsx
@@ -34,7 +34,8 @@ function Login() {
       </div>
       <div className="form-section">
         <div className="form">
-          <h1 className="title">Bienvenido</h1>
+          <h1 className="restaurant-name">Buen Sabor</h1>
+          <h2 className="title">Bienvenido</h2>
 
           <div>
              <p className="subtitle">No tienes una cuenta? 
