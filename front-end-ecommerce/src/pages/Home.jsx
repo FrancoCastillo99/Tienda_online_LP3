97690073d834059d2b7e79654482d68d144865b7
Lucas Chavez <chavezlucas04@gmail.com>
2024-09-15 20:06:01 -0300

    Actualizar contenido en la rama cambios

---
+++ front-end-ecommerce/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+import ProductCardArticles from '../components/ProductCard/ProductCardArticles';
+
+const Home = () => {
+  return (
+    <main>
+        <ProductCardArticles/>
+    </main>
+  );
+};
+
+export default Home;
