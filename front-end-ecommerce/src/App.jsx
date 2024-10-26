7f520d5948e7497aff2673b876f624ec1ed93b8e
Lucas Chavez <chavezlucas04@gmail.com>
2024-10-26 07:14:11 -0300

    26/10/2024 - 09:10

--- front-end-ecommerce/src/App.jsx
+++ front-end-ecommerce/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import RoutesConfig from './routes'
-import { CartProvider } from './context/CartContext.jsx';
+import { CartProvider } from '../src/pages/client/context/CartContext';
 
 function App() {
   return (
---
+++ front-end-ecommerce/src/pages/admin/views/home/AdminHome.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import SideBar from "../../components/SideBar/SideBar";
+import './AdminHome.css';
+
+const AdminHome = () => {
+    return (
+        <main>
+            <div className="home-container-admin">
+                <SideBar/>
+                <Outlet />
+            </div>
+        </main>
+    );
+}
+
+export default AdminHome;
